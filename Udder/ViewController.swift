c4e0585d20380efb8a0a673bc206f975dcc9f3dd
edwarfernandomartinezcastro <edwar.martinez@periferia-it.com>
2022-02-09 10:49:54 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by EDWAR FERNANDO MARTINEZ CASTRO on 9/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
